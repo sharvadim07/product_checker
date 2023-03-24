b59d6ccb6c028e27d5564917a95e39164eeb89cf
Vadim <sharvadim07@yandex.ru>
2023-03-24 13:00:31 +0700

    Use new version tesseract

--- install.sh
+++ install.sh
@@ -12,9 +12,18 @@ apt update && apt upgrade -y
 # Install dependencies with apt
 apt install -y build-essential zlib1g-dev \
 libncurses5-dev libgdbm-dev libnss3-dev libssl-dev \
-libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
-apt install -y git wget curl pkg-config sqlite3 tesseract-ocr
-apt install ffmpeg libsm6 libxext6  -y
+libreadline-dev libffi-dev libsqlite3-dev libbz2-dev
+apt install -y automake ca-certificates g++ git libtool libleptonica-dev make pkg-config wget curl
+apt install -y sqlite3
+apt install -y ffmpeg libsm6 libxext6
+
+# Add new repo for tesseract
+apt install -y curl
+echo "deb https://notesalexp.org/tesseract-ocr-dev/bullseye/ bullseye main" \
+| tee /etc/apt/sources.list.d/notesalexp.list > /dev/null
+curl -sSL https://notesalexp.org/debian/alexp_key.asc | apt-key add -
+apt update
+apt install -y tesseract-ocr  
 
 # Clean up
 apt clean && rm -rf /var/lib/apt/lists/*
